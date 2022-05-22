1f8e423f0c9d9cf5a7662266dffd8912d4a42204
jesusmb230795 <jesusmiranda2307@gmail.com>
2022-05-22 02:17:24 -0500

    Se añaden funciones de preprocessing

--- src/opt2/preprocessing.py
+++ src/opt2/preprocessing.py
@@ -147,3 +147,90 @@ class data_to_graph():
         return self
 
 
+def get_data(data_dir, date=date.today().strftime("%Y-%m-%d"), tam_data=100, price='Open'):
+    """
+    Obtiene datos de un directorio de una fecha dada
+    param:
+    data_dir: directorio de datos
+    date: fecha a analizar, por default toma la fecha actual si no se especifica
+    tam_data: analiza cierto tamaño del dataset de forma aleatoria, por default son 100 monedas a analizar
+    price: selecciona con que precio se hará el ejercicio(High,Low,Open,Close), por default es Open
+    return:
+    dataframe: con datos especificados
+    """
+    df = pd.read_csv(data_dir)
+    df_date = df[df['Date']==date]
+    df_date = df_date[df_date[price] > 0]
+    
+    if(tam_data == None):
+        df_random = df_date
+    else:
+        df_random = df_date.sample(n = tam_data)
+    
+    
+    df_random = df_random.reset_index()
+    data = df_random[["ticker", price]]
+    data.columns = ['Símbolo', 'Precio']
+    
+    return data    
+
+def exchange_rate_matrix(data):
+    """
+    Exchange Rate Matrix Representation
+    param:
+        dataframe
+    return:
+        dataframe
+    """
+    n = data.shape[0]
+
+    max_spread_pct = 0.05 # maximum bid-ask spread in pct of bid, 0.05 for 5%
+
+    c1 = data[['Precio']]
+    aux = c1.copy()
+    random.seed(10)
+    for i in range(n):
+        c1[i] = aux/c1[['Precio']].values[i]*(1+random.uniform(0,max_spread_pct))
+    c1.drop(columns=['Precio'],inplace=True)
+    for i in range(len(c1.index)):
+        for j in range(len(c1.columns)):
+            if i==j:
+                c1.loc[i,j] = 1
+    return c1
+
+def log_transformed_rep(data):
+    """
+    Log-Transformed Representations
+    param:
+        dataframe
+    return:
+        dataframe
+    """
+    
+    df_ln = np.round(-np.log(data),2)
+    return df_ln
+
+def create_grap(data):
+    """
+    Crea grafo a partir de los datos de cripomonedas y precio
+    param:
+        dataframe
+    return:
+        grafo
+    """
+    df = exchange_rate_matrix(data)
+    df_ln = log_transformed_rep(df)
+    
+    n = df_ln.shape[0]
+    
+    edge = []
+    # Covert to formatto use un graph
+    for i in range(n):
+        for j in range(n):
+            if (i != j):
+                edge.append([str(i), str(j), df_ln.loc[i][j]])
+
+    G = nx.DiGraph()        
+    G.add_weighted_edges_from(edge)
+    
+    return G
